a286cf7009d3839dcff564b3f783fab4015a442b
RoKerjea <rokerjea@student.42.fr>
2023-07-30 19:30:15 +0200

    done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use std::io::stdin;
 
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_lowercase(),
+        }
+    }
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
@@ -9,18 +27,29 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
-    let mut allow_them_in = false;
-    let visitor_list = ["bert", "steve", "fred"];
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_them_in = true;
+	let mut visitor_list = vec![
+            Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+            Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
+            Visitor::new("fred", "Wow, who invited Fred?"),
+        ];
+    loop {
+        println!("Hello, what's your name? (Leave empty and press ENTER to quit)");
+        let name = what_is_your_name();
+
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", &name);
+                    visitor_list.push(Visitor::new(&name, "New friend"));
+                }
+            }
         }
     }
-    if allow_them_in {
-        println!("Welcome to the Treehouse, {}", name);
-    } else {
-        println!("Sorry, you aren't on the list.");
-    }
+
+    println!("The final list of visitors:");
+    println!("{:#?}", visitor_list);
 }
